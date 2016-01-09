2ad40d41a041b8b7f7994aca987cad775ec0b27e
softpand <atta.rehman8123@gmail.com>
2016-01-10 01:08:46 +0500

    Use Optional Parameter in typescript

--- optional_function/main.js
+++ optional_function/main.js
@@ -1,4 +1,24 @@
+//function with name , optional parameter use in named function 
 function add(a, b, c) {
-    return a + b;
+    if (c)
+        return a + b + c;
+    else
+        return a + b;
 }
 console.log(add(2, 1));
+//we can also define the parameter values on declaring parameter time 
+function sub(a, b, c) {
+    if (b === void 0) { b = 2; }
+    if (c === void 0) { c = a - b; }
+    return c;
+}
+console.log(sub(5, 3));
+//Assign a string value in any parameter variable in anonymous function 
+var check = function (FirstName, LastName) {
+    if (LastName === void 0) { LastName = "Ur Rheman"; }
+    if (LastName && LastName.length)
+        return FirstName + " " + LastName;
+    else
+        return FirstName;
+};
+console.log(check("Atta"));
--- optional_function/main.ts
+++ optional_function/main.ts
@@ -1,4 +1,25 @@
+//function with name , optional parameter use in named function 
 function add(a:number , b:number , c?:number){
-	return a+b
+	if(c)
+	return a+b+c;
+	else
+	return a+b;
 }
 console.log(add(2,1));
+
+
+//we can also define the parameter values on declaring parameter time 
+function sub(a:number , b:number=2 , c=a-b){
+	return c
+}
+console.log(sub(5,3))
+
+//Assign a string value in any parameter variable in anonymous function 
+ 
+var check:(FirstName : string , LastName?:string)=>string = function (FirstName : string , LastName:string = "Ur Rheman"):string{
+	if(LastName && LastName.length)
+	return FirstName+" "+LastName;
+	else
+	return FirstName;
+}
+console.log(check("Atta"))
